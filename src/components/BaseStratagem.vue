8af368d3b897a7743397f29844c51bd3602c0810
t2vee <68762600+t2vee@users.noreply.github.com>
2024-07-14 15:14:54 +1000

    added base stratagem icon component

---
+++ src/components/BaseStratagem.vue
@@ -0,0 +1,14 @@
+<!-- Copyright (c) 2024 t2vee. All rights reserved.
+Use of this source code is governed by an MPL license. -->
+
+<script setup lang="ts">
+defineProps({
+  name: {type: String, required: true},
+  icon: {type: String, required: true},
+  active: {type: Boolean, required: false, default: false}
+});
+</script>
+
+<template>
+  <img :class="active ? 'w-[128px] border-hdgold border-2' : 'w-[64px]'" class="aspect-square" :src="icon" :alt="name">
+</template>
